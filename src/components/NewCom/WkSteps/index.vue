4dc915bbef4ce8160dc641a188ae6cbcbc22639b
ask <2506377990@qq.com>
2026-08-14 09:16:46 +0800

    first commit

---
+++ src/components/NewCom/WkSteps/index.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Description: 恒河制药软件
+ * @Author: 恒河制药
+ * @LastEditors: yang
+-->
+<template>
+  <flexbox class="wk-steps" align="stretch">
+    <slot />
+  </flexbox>
+</template>
+
+<script>
+
+export default {
+  // 步骤说明
+  name: 'WkSteps',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+
+  beforeDestroy() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.wk-steps {
+  position: relative;
+  text-align: center;
+}
+</style>
---
+++ src/config.js
@@ -0,0 +1,19 @@
+const getLocationOrigin = () => {
+  return (
+    window.location.protocol +
+    '//' +
+    window.location.hostname +
+    (window.location.port ? ':' + window.location.port : '')
+  )
+}
+
+const companyName = '恒河制药CRM'
+const version = 'V11.3.3'
+const baiduKey = '百度key'
+
+export default {
+  version,
+  companyName,
+  getLocationOrigin,
+  baiduKey
+}
